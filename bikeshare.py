423eb0c33f56200a5c71f1918d8db5f911d52f0a
Bolatito Osiname <osinamemichael@yahoo.com>
2021-12-17 00:14:36 +0100

    Refactoring the select days input

--- bikeshare.py
+++ bikeshare.py
@@ -39,7 +39,7 @@ def get_filters():
     # get user input for day of week (monday, tuesday, ... sunday or none)
     while True:
         days= ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday','None']
-        day = input("\n Which day of the week would you like to consider? (Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)? Type 'None' for no day filter \n").title()
+        day = input("\n Which day of the week would you like to consider? (Mon, Tue, Wed, Thur, Fri, Sat, Sun)? Type 'None' for no day filter \n").title()
         if day in days:
             break
         else:
